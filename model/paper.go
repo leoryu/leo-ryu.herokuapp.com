cf26b1c0933dd79a28818a9869d97f1da70b64ea
leoryu <leo-ryu@outlook.com>
2018-12-04 06:54:10 +0000

    modify model.Paper

--- model/paper.go
+++ model/paper.go
@@ -1,41 +1,4 @@
-package server
-
-import (
-	"net/http"
-
-	"github.com/labstack/echo"
-	"github.com/leoryu/leo-ryu.herokuapp.com/model"
-	"github.com/leoryu/leo-ryu.herokuapp.com/store"
-)
-
-func PubilushPaper(c echo.Context) error {
-	paper := new(model.Paper)
-	if err := c.Bind(paper); err != nil {
-		return err
-	}
-	if c.Request().Method == echo.PUT {
-		return store.ModifyPaper(c, paper)
-	}
-	return store.SavePaper(c, paper)
-}
-
-func DeletePaper(c echo.Context) error {
-	id := c.Param("id")
-	return store.DeletePaper(c, id)
-}
-
-func GetPaper(c echo.Context) error {
-	id := c.Param("id")
-	paper, err := store.GetPaper(c, id)
-	if err != nil {
-		return err
-	}
-	if paper == nil {
-		return echo.ErrNotFound
-	}
-	return c.JSON(http.StatusOK, paper)
-}
-ackage model
+package model
 
 import (
 	"time"
@@ -50,3 +13,4 @@ type Paper struct {
 	CreatedAt time.Time `json:"created_at"`
 	EditedAt  time.Time `json:"edited_at"`
 }
+
