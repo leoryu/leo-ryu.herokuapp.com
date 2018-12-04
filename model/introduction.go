cf227f57bfe42b0ac3823c7c28898f8a526cd13e
leoryu <leo-ryu@outlook.com>
2018-12-04 11:35:23 +0000

    add introduction

---
+++ model/introduction.go
@@ -0,0 +1,7 @@
+package model
+
+type Introduction struct {
+	Title    string `json:"title"`
+	Subject  string `json:"subject"`
+	Abstract string `json:"abstract"`
+}
--- model/paper.go
+++ model/paper.go
@@ -4,14 +4,10 @@ import (
 	"time"
 )
 
-// Paper is used for paper management
 type Paper struct {
-	ID        int       `json:"id"`
-	Title     string    `json:"title"`
-	Subject   string    `json:"subject"`
-	Abstract  string    `json:"abstract"`
+	*Introduction
+	ID        string    `json:"id"`
 	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"created_at"`
 	EditedAt  time.Time `json:"edited_at"`
 }
-
