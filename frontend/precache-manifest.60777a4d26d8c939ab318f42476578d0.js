01e742bfbe7671d478397b8151452af9a6cb3f42
leo <leo-ryu@outlook.com>
2019-04-21 22:27:42 +0800

    add frontend dir

---
+++ frontend/precache-manifest.60777a4d26d8c939ab318f42476578d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4349fef60dfcebffc883",
+    "url": "/static/js/main.2173d8e8.chunk.js"
+  },
+  {
+    "revision": "738d7009828e5a5c8a3e",
+    "url": "/static/js/2.eede9e0d.chunk.js"
+  },
+  {
+    "revision": "4349fef60dfcebffc883",
+    "url": "/static/css/main.a5527d57.chunk.css"
+  },
+  {
+    "revision": "3b024c9b1ef08b01774c13152ed4b8ad",
+    "url": "/index.html"
+  }
+];
