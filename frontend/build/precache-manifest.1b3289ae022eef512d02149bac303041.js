17d4c20250aa42946cc62e4ed4e285dd1f1bfe2e
leo <leo-ryu@outlook.com>
2019-04-25 23:11:38 +0800

    change frontend

---
+++ frontend/build/precache-manifest.1b3289ae022eef512d02149bac303041.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "deb21a1227a221a57353",
+    "url": "/static/js/main.f87fffb3.chunk.js"
+  },
+  {
+    "revision": "15735ccafd1914f1849a",
+    "url": "/static/js/2.f8a1607a.chunk.js"
+  },
+  {
+    "revision": "deb21a1227a221a57353",
+    "url": "/static/css/main.a5527d57.chunk.css"
+  },
+  {
+    "revision": "b84b6abdcf153b210662c1ef5dc612c5",
+    "url": "/index.html"
+  }
+];
